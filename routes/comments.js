d691a6d2d1c19fb7647a45fb9713680d4b89f33e
Gray Smith <gray.smith00@gmail.com>
2018-02-09 16:01:23 +0000

    add edit and update functionality for comments restricted to the authorized owner of that comment

--- routes/comments.js
+++ routes/comments.js
@@ -49,6 +49,81 @@ router.post("/", isLoggedIn, function(req, res) {
    });
 });
 
+// COMMENT EDIT PAGE
+// ======================================================
+router.get('/:comment_id/edit', function(req, res) {
+    
+    // find the campground by id
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            // find comment by ID
+            Comment.findById(req.params.comment_id, function(err, foundComment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    
+                    //res.render("comments/edit", {campground: foundCampground, comment: foundComment});
+                    // does the user own the comment?
+                    if (req.isAuthenticated()) {
+                        if (foundComment.author.id.equals(req.user.id)) {
+                            res.render("comments/edit", {campground: foundCampground, comment: foundComment});
+                        } else {
+                            res.send("bro u cant edit other peoples comments, what u thinkin");
+                        }
+                    } else {
+                        res.send("u gotta be logged in for that one homie");
+                    }
+                }
+            });
+        }
+    });
+});
+
+// COMMENT UPDATE ACTION
+// ======================================================
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            console.log(err);
+            res.redirect("campgrounds");
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
+
+// COMMENT DESTROY ACTION
+// ======================================================
+
+
+// CHECK COMMENT OWNERSHIP
+// ======================================================
+// function checkCommentOwnership (req, res, next) {
+//     // is the user logged in?
+//     if (req.isAuthenticated()) {
+//         // find the campground by ID
+//         Campground.findById(req.params.id, function(err, foundCampground) {
+//             if (err) {
+//                 console.log(err);
+//                 res.redirect("back");
+//             } else {
+//                 // does the user own the campground?
+//                 if(foundCampground.author.id.equals(req.user._id)) {
+//                     // congratulations you have passed the test
+//                     next();
+//                 } else {
+//                     res.redirect("back");
+//                 }
+                
+//             }
+//         });        
+//     } else {
+//         res.send("You need to be logged in to do that dawg!");
+//     }
+// }
+
 // AUTHENTICATION - isLoggedIn MIDDLEWARE
 // ======================================================
 function isLoggedIn(req, res, next) {
